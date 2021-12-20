15529260195f670b9b50637b9e8e23013f231d64
ELIJAH KIRAGU <eliki13720@gmail.com>
2021-12-20 08:16:50 +0300

    created the form with name, email and text area boxes

---
+++ bootstrap/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("div").css("border", "3px solid red");
+    // $('p').click(function(){
+    //     alert("hi there i am working")
+    //     // $("#whatWeDo1").html("<p>hi ther welcome</p>");
+    // });
+    // $('p').click(function(){
+    //     alert("this is a paragraph")
+    // })
+
+})
+    
